f85fe849333c3c8965fdba0221f365f441dc856e
JulyPardo <julypardorauiz@gmail.com>
2021-08-02 20:38:08 -0500

    Project creation

---
+++ Pokemon-SwiftUI/Pokemon-SwiftUI/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Pokemon-SwiftUI
+//
+//  Created by July on 2/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardPokemon()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
